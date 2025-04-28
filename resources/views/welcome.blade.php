d5cc17c6e6f362ed627d9e5bb582cc524cdd2be4
Marcelo Santos <mjdos.2014@gmail.com>
2025-04-28 11:28:27 -0300

    Ajuste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,12 @@
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
-                    <h1 class="mb-1 font-medium">Seja bem vindo ao site do ShellForge</h1>
+                  
+                    <ul class="flex gap-3 text-sm leading-normal">
+                        <li>
+                            <font size=3><b>Seja bem vindo ao site do ShellForge!</b></font><br><br>
+                        </li>
+                    </ul>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">O site está em fase de desenvolvimento, e será lançado em 01 de Agosto de 2025 juntamente com:</p>
                     
                     <ul class="flex gap-3 text-sm leading-normal">
